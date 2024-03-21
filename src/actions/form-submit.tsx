6758790462dc03d0746549eb80b349f7b29f769c
Utkarsh <utkarshjha.4009@gmail.com>
2024-03-21 20:40:15 +0530

    Creating new user

--- src/actions/form-submit.tsx
+++ src/actions/form-submit.tsx
@@ -6,9 +6,25 @@ interface Response {
   };
 }
 
+import { db } from "@/server/db";
 import { UserSchema } from "@/lib/types";
 
-export const CreateUser = async (data: unknown): Promise<Response> => {
+export const CreateUser = async (data: any): Promise<Response> => {
+  const {id, name, email, phone, roll, tryhackmeId, year, rate} = data;
+  const existingUser = await db.user.findUnique({ where: { email } });
+  if (existingUser) return { error: "User already exists" };
+  const newUser = await db.user.create({
+    data: {
+      id: id,
+      name: name,
+      email: email,
+      phone: phone,
+      roll: roll,
+      tryhackmeId: tryhackmeId,
+      year: year,
+      rate: rate,
+    },
+  });
   return { success: { message: "success" } };
   // const result = UserSchema.safeParse(data);
   // if (result.success) {
